c82e6559be3af2f78cce38f8c20132f93cffa3dc
jasmeendesai <130737953+jasmeendesai@users.noreply.github.com>
2023-11-02 07:34:41 +0000

    JS datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we get error  alert is not defined //we are using nodejs, not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+let name = "jas" //string;
+let age = 18 //number
+let isLoggedIn = false //boolean
+let state = null
+
+
+// Primitive DataTypes
+// number => 2 to power 53
+// bigint => higher values 
+// string => ""
+// boolean => true/false
+// null => In Js it is standalone value => representation of an empty value => variable declared but assigned value null
+// undefined => variable is declared value is not defined 
+// symbol => used in react for uniqueness
+
+// Non Primitive DataTypes
+// object 
+
+console.log(typeof null); // object -- it is language error
+console.log(typeof undefined); //undefined-- bcz undefined is a type in Js
